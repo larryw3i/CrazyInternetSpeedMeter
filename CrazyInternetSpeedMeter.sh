629481687dcb8fc39af3873a83acf17d47128887
larryw3i <larryw3i@163.com>
2024-06-29 01:48:10 +0800

    Daily copy.

--- CrazyInternetSpeedMeter.sh
+++ CrazyInternetSpeedMeter.sh
@@ -5,7 +5,6 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
-# SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 EXTENSION_NAME="CrazyInternetSpeedMeter"
 PROJECT_DIR="${PWD}"
 SRC_DIR="${PWD}/src"
@@ -40,61 +39,9 @@ if [[ "$(id -u)" -eq 0 ]]; then
     INSTALL_DIR="/usr/share/gnome-shell/extensions"
 fi
 
-# print <arg>
-print() {
-    echo -e "${NC}[+] ${1}${NC}"
-    echo -e "[+] ${1}" &>>"$LOG_FILE"
-}
-
-# print_warning <arg>
-print_warning() {
-    echo -e "${NC}[${YELLOW}!${NC}] ${1}${NC}"
-    echo -e "[!] ${1}" &>>"$LOG_FILE"
-}
-
-# print_failed <arg>
-print_failed() {
-    echo -e "${NC}[${RED}x${NC}] ${1}${NC}"
-    echo -e "[x] ${1}" &>>"$LOG_FILE"
-}
-
-# print_success <arg>
-print_success() {
-    echo -e "${NC}[${GREEN}\xE2\x9C\x94${NC}] ${1}${NC}"
-    echo -e "[✔] ${1}" &>>"$LOG_FILE"
-}
-
-# print_suggestion <arg>
-print_suggestion() {
-    echo -e "${NC}[${BLUE}#${NC}] ${1}${NC}"
-    echo -e "[#] ${1}" &>>"$LOG_FILE"
-}
-
-# is_failed <success_message> <failed_message>
-is_failed() {
-    if [[ "$?" -eq 0 ]]; then
-        print_success "${1}"
-    else
-        print_failed "${2}"
-    fi
-}
-
-# is_warning <success_message> <warning_message>
-is_warning() {
-    if [[ "$?" -eq 0 ]]; then
-        print_success "${1}"
-    else
-        print_warning "${2}"
-    fi
-}
-
 make_test() {
     install_extension
     echo "Start testing. . ."
-    # export G_MESSAGES_DEBUG=backtrace-segfaults
-    export MUTTER_DEBUG_DUMMY_MODE_SPECS=1366x768
-    # export SHELL_DEBUG=backtrace-warnings
-    export SHELL_DEBUG=backtrace-segfaults
     dbus-run-session -- gnome-shell --nested --wayland
 }
 
@@ -106,38 +53,6 @@ install_extension() {
         --force \
         ${DEFAULT_PACK_FILE}
     echo "${DEFAULT_PACK_FILE} installed."
-}
-
-# install extension
-install_v0() {
-    print "Installing to ${INSTALL_DIR}"
-    update_version_name
-    # compile_schemas
-    mkdir -p "${INSTALL_DIR}"
-    rm -rf "${INSTALL_DIR}/${EXTENSION_FULL_NAME}"
-    cp \
-        -rf \
-        "${SRC_DIR}" \
-        "${INSTALL_DIR}/${EXTENSION_FULL_NAME}" \
-        &>>"$LOG_FILE"
-    is_failed \
-        "Done" \
-        "Skipping: Can not install to ${INSTALL_DIR}. See log for more info."
-}
-
-# build for release
-build() {
-    print "Creating ${EXTENSION_FULL_NAME}.zip"
-    update_version_name
-    mkdir -p "${OUT_DIR}"
-    zip \
-        -6rXj \
-        "${OUT_DIR}/${EXTENSION_FULL_NAME}.zip" \
-        "${SRC_DIR}" \
-        &>>"$LOG_FILE"
-    is_failed \
-        "Done" \
-        "Skipping: Creating zip is failed. See log for more info."
 }
 
 compile_schemas() {
@@ -190,15 +105,10 @@ pack_extension() {
 
 update_version_name() {
     METADATA_FILE_CP=${METADATA_FILE/.json/.0.json}
-    # cp ${METADATA_FILE} ${METADATA_FILE_CP}
     jq \
         ".\"version-name\" |= \"$(date -u +%Y%m%d.%H%M)\"" \
         ${METADATA_FILE} \
         >${METADATA_FILE_CP}
-    # jq \
-    #     ".\"version\" |= \"$(date +%Y%m%d.%H%M)\"" \
-    #     ${METADATA_FILE_CP} \
-    #     >${METADATA_FILE_CP}
 
     cp ${METADATA_FILE_CP} ${METADATA_FILE}
     diff_test=$(diff ${METADATA_FILE} ${METADATA_FILE_CP})
